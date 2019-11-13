8983279d606cd73de69e9d2ac182cc31338e7f94
Abhishek Shende <osdevisnot@gmail.com>
2019-11-13 09:35:24 -0800

    Auto install (#2)

---
+++ postinstall.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const sort = require("sort-package-json");
+const { name } = require("./package.json");
+
+try {
+  const target = "./package.json";
+  const pkg = JSON.parse(fs.readFileSync(target, "utf-8"));
+  if (typeof pkg.prettier === "undefined") {
+    pkg.prettier = name;
+    fs.writeFileSync(target, JSON.stringify(sort(pkg), null, "	"), "utf-8");
+  }
+} catch (e) {
+  console.log(e);
+}
