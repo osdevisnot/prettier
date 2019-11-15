7c7e4e887d357b5221d12b06a1a708debc5133f5
Abhishek Shende <osdevisnot@gmail.com>
2019-11-15 12:57:16 -0800

    remove debug console log

--- postinstall.js
+++ postinstall.js
@@ -6,7 +6,6 @@ const { name } = require("./package.json");
 try {
   const target = join(process.cwd(), "..", "..", "..", "package.json");
   const pkg = JSON.parse(fs.readFileSync(target, "utf-8"));
-  console.log("TCL: pkg", pkg);
   if (typeof pkg.prettier === "undefined") {
     pkg.prettier = name;
     fs.writeFileSync(target, JSON.stringify(sort(pkg), null, "	"), "utf-8");
