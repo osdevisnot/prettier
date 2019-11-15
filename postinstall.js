17cce357bb9eef16e4dcc04b4d9dc7875b36d4c1
Abhishek Shende <osdevisnot@gmail.com>
2019-11-15 13:07:02 -0800

    remove existing configs

--- postinstall.js
+++ postinstall.js
@@ -1,15 +1,20 @@
-const { join } = require("path");
-const fs = require("fs");
-const sort = require("sort-package-json");
-const { name } = require("./package.json");
+let { join } = require("path");
+let { readFileSync, writeFileSync, unlinkSync } = require("fs");
+let sort = require("sort-package-json");
+let { name } = require("./package.json");
 
 try {
-  const target = join(process.cwd(), "..", "..", "..", "package.json");
-  const pkg = JSON.parse(fs.readFileSync(target, "utf-8"));
+  let root = join(process.cwd(), "..", "..", "..");
+  let target = join(root, "package.json");
+  let pkg = JSON.parse(readFileSync(target, "utf-8"));
   if (typeof pkg.prettier === "undefined") {
     pkg.prettier = name;
-    fs.writeFileSync(target, JSON.stringify(sort(pkg), null, "	"), "utf-8");
+    writeFileSync(target, JSON.stringify(sort(pkg), null, "	"), "utf-8");
   }
-} catch (e) {
-  console.error(e);
-}
+  for (let file in [".prettierrc", "prettier.config.js"]) {
+    let target = join(root, file);
+    if (fs.existsSync(target)) {
+      unlinkSync(target);
+    }
+  }
+} catch (e) {}
