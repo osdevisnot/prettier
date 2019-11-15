15feeca19ec70289aa55f8a18bbeec25af3ca230
Abhishek Shende <osdevisnot@gmail.com>
2019-11-15 11:52:04 -0800

    debug

--- postinstall.js
+++ postinstall.js
@@ -3,8 +3,9 @@ const sort = require("sort-package-json");
 const { name } = require("./package.json");
 
 try {
-  const target = "./package.json";
+  const target = "package.json";
   const pkg = JSON.parse(fs.readFileSync(target, "utf-8"));
+  console.log('TCL: pkg', pkg);
   if (typeof pkg.prettier === "undefined") {
     pkg.prettier = name;
     fs.writeFileSync(target, JSON.stringify(sort(pkg), null, "	"), "utf-8");
