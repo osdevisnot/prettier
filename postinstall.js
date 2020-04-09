54b81008c76cf4326951c793ea7544733e4acce5
Abhishek Shende <abhisheksh@zillowgroup.com>
2020-04-09 16:21:02 -0700

    add previse commit and husky

--- postinstall.js
+++ postinstall.js
@@ -7,10 +7,12 @@ try {
   let root = join(process.cwd(), '..', '..', '..');
   let target = join(root, 'package.json');
   let pkg = JSON.parse(readFileSync(target, 'utf-8'));
-  if (typeof pkg.prettier === 'undefined') {
-    pkg.prettier = name;
-    writeFileSync(target, JSON.stringify(sort(pkg), null, '	') + '\n', 'utf-8');
-  }
+
+  pkg.prettier = name;
+  pkg.husky = { hooks: { 'pre-commit': 'precise-commits' } };
+
+  writeFileSync(target, JSON.stringify(sort(pkg), null, '	') + '\n', 'utf-8');
+
   for (let file of ['.prettierrc', 'prettier.config.js']) {
     target = join(root, file);
     if (existsSync(target)) unlinkSync(target);
