b669bee7bf0886cebc62b16890c95e0132591e64
Abhishek Shende <abhisheksh@zillowgroup.com>
2020-04-09 16:54:31 -0700

    update package.json only when changed.

--- postinstall.js
+++ postinstall.js
@@ -7,11 +7,20 @@ try {
   let root = join(process.cwd(), '..', '..', '..');
   let target = join(root, 'package.json');
   let pkg = JSON.parse(readFileSync(target, 'utf-8'));
+  let modified = false;
 
-  pkg.prettier = name;
-  pkg.husky = { hooks: { 'pre-commit': 'precise-commits' } };
+  if (typeof pkg.prettier === 'undefined') {
+    pkg.prettier = name;
+    modified = true;
+  }
+  if (typeof pkg.husky === 'undefined') {
+    pkg.husky = { hooks: { 'pre-commit': 'precise-commits' } };
+    modified = true;
+  }
 
-  writeFileSync(target, JSON.stringify(sort(pkg), null, '	') + '\n', 'utf-8');
+  if (modified) {
+    writeFileSync(target, JSON.stringify(sort(pkg), null, '	') + '\n', 'utf-8');
+  }
 
   for (let file of ['.prettierrc', 'prettier.config.js']) {
     target = join(root, file);
